24d2d98b12797f6e6f0f87814a005cc638b78dc1
Appsius <ilyas.ustunkaya234434@gmail.com>
2020-02-24 21:26:25 +0300

    Joining Paths

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const fs = require('fs');
 const util = require('util');
 const chalk = require('chalk');
+const path = require('path');
 
 // Method #2
 // const lstat = util.promisify(fs.lstat);
@@ -17,7 +18,7 @@ fs.readdir(targetDir, async (err, filenames) => {
   }
 
   const statPromises = filenames.map(filename => {
-    return lstat(filename);
+    return lstat(path.join(targetDir, filename));
   });
 
   const allStats = await Promise.all(statPromises);
